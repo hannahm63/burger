bd57a548444694bb9f6944038c04e56803731381
hannahm63 <52358146+hannahm63@users.noreply.github.com>
2019-11-16 23:05:45 -0700

    fixed async issue with callback functions and completed required orm functions

--- config/orm.js
+++ config/orm.js
@@ -2,30 +2,36 @@ const connection = require("./connection");
 
 const orm = {
 
-    selectAll: function (tableName) {
+    selectAll: function (tableName, cb) {
 
         const queryString = "SELECT * FROM ??";
 
         connection.query(queryString, [tableName], function (error, results) {
             if (error) throw error;
-            return results;
-        })
+            cb(results);
+        });
     },
 
-    insertOne: function (tableName, newBurger) {
+    insertOne: function (tableName, newBurger, cb) {
 
         const queryString = "INSERT INTO ?? SET ?";
 
         connection.query(queryString, [tableName, newBurger], function (error, results) {
             if (error) throw error;
-
-
-
-        })
+            cb(results);
+        });
 
     },
 
-    updateOne: function () {
+    updateOne: function (tableName, burgerUpdate, columnName, columnValue, cb) {
+        const queryString = "UPDATE ?? SET ? WHERE ?? = ?";
+
+        connection.query(queryString, [tableName, burgerUpdate, columnName, columnValue], function (error, results) {
+            if (error) throw error;
+            cb(results);
+        });
 
     }
-}
+};
+
+module.exports = orm;
